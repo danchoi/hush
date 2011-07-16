d1124eff6c354d427bf300fd2fc07e71d4d64568
Daniel Choi <dhchoi@gmail.com>
2011-07-16 02:37:07 -0400

    the pipe stuff doesn't work too well

--- HuluAutomuter/AppDelegate.rb
+++ HuluAutomuter/AppDelegate.rb
@@ -10,10 +10,12 @@
 NSLog `pwd `
 NSLog `find .  `
 
+puts "Hello world"
 class AppDelegate
-    attr_accessor :window, :status_item, :task
+    attr_accessor :window, :status_item, :task, :file
     def applicationDidFinishLaunching(a_notification)
         # Insert code here to initialize your application
+      `killall hulu-automuter`
       self.status_item = NSStatusBar.systemStatusBar.
         statusItemWithLength NSVariableStatusItemLength
       status_item.highlightMode = true
@@ -37,6 +39,14 @@ class AppDelegate
       task.setLaunchPath automuter_path
       task.setStandardOutput output
       task.setStandardError error
+      self.file = self.task.standardOutput.fileHandleForReading
+      nc = NSNotificationCenter.defaultCenter
+      nc.addObserver(self,
+                     selector: "data_ready:",
+                     name: NSFileHandleReadCompletionNotification,
+                     object: file)
+      
+      file.readInBackgroundAndNotify
       task.launch
       pid = task.processIdentifier
       puts "PID: #{pid}"
@@ -47,8 +57,17 @@ class AppDelegate
       NSLog "testAction"
     end
 
+    def data_ready(notification)
+      return
+      data = notification.userInfo[NSFileHandleNotificationDataItem]
+      output = NSString.alloc.initWithData(data, encoding: NSUTF8StringEncoding)
+      NSLog "output: #{output}"
+      self.file.readInBackgroundAndNotify 
+    end
+
     def applicationWillTerminate(application)
       self.task.terminate
+      `killall hulu-automuter`
     end
 end
 
