0ea304c52ac11bc1f967c9a3d2c2d7076a5faee1
Daniel Choi <dhchoi@gmail.com>
2011-07-18 22:25:08 -0400

    try using full proxy; but errors

--- HuluAutomuter/AppDelegate.rb
+++ HuluAutomuter/AppDelegate.rb
@@ -7,57 +7,47 @@
 #
 
 
-NSLog `pwd `
-NSLog `find .  `
-
-puts "Hello world"
 class AppDelegate
-    attr_accessor :window, :status_item, :task, :file
-    def applicationDidFinishLaunching(a_notification)
-        # Insert code here to initialize your application
-      `killall hulu-automuter`
-      `networksetup -setwebproxy AirPort 127.0.0.1 8123`
-      `networksetup -setwebproxystate AirPort on`
+  attr_accessor :window, :status_item, :task, :file
+  def applicationDidFinishLaunching(a_notification)
+      # Insert code here to initialize your application
+    `killall automuter-osx`
+    self.status_item = NSStatusBar.systemStatusBar.
+      statusItemWithLength NSVariableStatusItemLength
+    status_item.highlightMode = true
+    status_item.title = "Hulu Automuter"
+    status_item.enabled = true
+    status_item.toolTip = "HuluAutomuter"
+    status_item.setAction :'testAction:'
+    status_item.setTarget self
+    automuter_path = NSBundle.mainBundle.pathForResource("automuter-osx", ofType:"")
+    NSLog `ruby -v`
+    # `#{automuter_path} & `
+    #pid_file = "#{ENV['HOME']}/hulu.pid"
+    #pid = File.read pid_file
+    #puts "PID: #{pid}"
 
+    self.task = NSTask.alloc.init
+    output = NSPipe.alloc.init
+    error = NSPipe.alloc.init
 
-      self.status_item = NSStatusBar.systemStatusBar.
-        statusItemWithLength NSVariableStatusItemLength
-      status_item.highlightMode = true
-      status_item.title = "Hulu Automuter"
-      status_item.enabled = true
-      status_item.toolTip = "HuluAutomuter"
-      status_item.setAction :'testAction:'
-      status_item.setTarget self
-      NSLog "hello"
-      automuter_path = NSBundle.mainBundle.pathForResource("hulu-automuter", ofType:"")
-      NSLog `ruby -e "puts 'hi'"`
-      # `#{automuter_path} & `
-      #pid_file = "#{ENV['HOME']}/hulu.pid"
-      #pid = File.read pid_file
-      #puts "PID: #{pid}"
+    task.setLaunchPath automuter_path
+    task.setStandardOutput output
+    task.setStandardError error
 
-      self.task = NSTask.alloc.init
-      output = NSPipe.alloc.init
-      error = NSPipe.alloc.init
+    task.launch
+    pid = task.processIdentifier
+    puts "PID: #{pid}"
 
-      task.setLaunchPath automuter_path
-      task.setStandardOutput output
-      task.setStandardError error
+  end
 
-      task.launch
-      pid = task.processIdentifier
-      puts "PID: #{pid}"
+  def testAction(sender)
+    NSLog "testAction"
+  end
 
-    end
-
-    def testAction(sender)
-      NSLog "testAction"
-    end
-
-    def applicationWillTerminate(application)
-      self.task.terminate
-      `killall hulu-automuter`
-      `networksetup -setwebproxystate AirPort off`
-    end
+  def applicationWillTerminate(application)
+    self.task.terminate
+    `killall automuter-osx`
+  end
 end
 
