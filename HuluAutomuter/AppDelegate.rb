204c0e9e305f79ffac95040b00c688d6a8507d79
Daniel Choi <dhchoi@gmail.com>
2011-07-19 00:17:31 -0400

    still not working from inside mac app

--- HuluAutomuter/AppDelegate.rb
+++ HuluAutomuter/AppDelegate.rb
@@ -11,43 +11,42 @@ class AppDelegate
   attr_accessor :window, :status_item, :task, :file
   def applicationDidFinishLaunching(a_notification)
       # Insert code here to initialize your application
-    `killall automuter-osx`
     self.status_item = NSStatusBar.systemStatusBar.
       statusItemWithLength NSVariableStatusItemLength
     status_item.highlightMode = true
     status_item.title = "Hulu Automuter"
     status_item.enabled = true
     status_item.toolTip = "HuluAutomuter"
-    status_item.setAction :'testAction:'
+    status_item.setAction :'statusClicked:'
     status_item.setTarget self
     automuter_path = NSBundle.mainBundle.pathForResource("automuter-osx", ofType:"")
-    NSLog `ruby -v`
-    # `#{automuter_path} & `
-    #pid_file = "#{ENV['HOME']}/hulu.pid"
-    #pid = File.read pid_file
-    #puts "PID: #{pid}"
-
+    `mkdir #{ENV['HOME']}/.automuter`
+    `rm #{ENV['HOME']}/.automuter/automuter.log`
+    `touch #{ENV['HOME']}/.automuter/automuter.log`
+    #cmd = "#{automuter_path} > #{ENV['HOME']}/.automuter/automuter.log 2>&1 &"
+    #puts cmd
+    #`#{cmd}`
     self.task = NSTask.alloc.init
-    output = NSPipe.alloc.init
-    error = NSPipe.alloc.init
-
+    @log_path = "#{ENV['HOME']}/.automuter/automuter.log"
+    @output = NSFileHandle.fileHandleForWritingAtPath @log_path
+    if @output.nil?
+      puts "Failed to open stdout path #@log_path"
+    end
+    puts @output.class
     task.setLaunchPath automuter_path
-    task.setStandardOutput output
-    task.setStandardError error
-
+    task.setStandardOutput @output
+    task.setStandardError @output
     task.launch
-    pid = task.processIdentifier
-    puts "PID: #{pid}"
-
+    @pid = task.processIdentifier
+    puts "PID: #{@pid}"
   end
 
-  def testAction(sender)
-    NSLog "testAction"
+  def statusClicked(sender)
+    NSLog "statusClicked"
   end
 
   def applicationWillTerminate(application)
-    self.task.terminate
-    `killall automuter-osx`
+    `kill -INT #{@pid}`
   end
 end
 
